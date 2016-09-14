858500c696ff4a31d84e8a66b75c41be55e848ea
Paul JULIEN <julien.paul95@gmail.com>
2016-09-14 12:09:39 +0200

    validation.php

--- validation.php
+++ validation.php
@@ -3,10 +3,24 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
+if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])
+		&&
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+	$email = $_POST['email'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	if(isset($_POST['email']) && isset($_POST['password'])){
+		if(!empty($_POST['email']) && !empty($_POST['password'])){
+			if(isEmailAvailable($db, $email) && isUsernameAvailable($db, $username)){
+				userRegistration($db, $username, $email,$password);
+				header('Location: login.php');
+			}
+			else{
+				$error = 'Un champ à déjà été utilisé';
+			}
+		}
+	}
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
